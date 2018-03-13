83c2b6082afec104f95627860c194dc2b471a5f3
Jimmy Guzman <jimmy.guzman@outlook.com>
2018-03-13 13:43:39 -0500

    added firebase config and build dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 var path = require("path");
-// create index.html file and put in the dist folder and include index_bundle.js
 var HtmlWebpackPlugin = require("html-webpack-plugin");
+var webpack = require("webpack");
 
-module.exports = {
+var config = {
   entry: "./app/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -24,3 +24,16 @@ module.exports = {
     })
   ]
 };
+
+if (process.env.NODE_ENV === "production") {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  );
+}
+
+module.exports = config;
