d5795b7e9219bb71398edb558de4a064dd661aeb
Jimmy Guzman <jimmy.guzman@outlook.com>
2018-03-14 16:23:35 -0500

    refactored to async/await

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -65,12 +65,11 @@ class Popular extends React.Component {
     this.updateLanguage(this.state.selectedLanguage);
   }
 
-  updateLanguage = lang => {
+  updateLanguage = async lang => {
     this.setState(() => ({ selectedLanguage: lang, repos: null }));
 
-    fetchPopularRepos(lang).then(repos => {
-      this.setState(() => ({ repos }));
-    });
+    const repos = await fetchPopularRepos(lang);
+    this.setState(() => ({ repos }));
   };
   render() {
     const { selectedLanguage, repos } = this.state;
